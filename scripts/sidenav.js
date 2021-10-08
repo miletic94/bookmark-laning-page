6f886fb822cf2f9c6e78cf22bbf0f388223674cc
miletic94 <mileticnemanja.lab@gmail.com>
2021-10-08 21:47:12 +0200

    Finish navbar & Hero Section.

---
+++ scripts/sidenav.js
@@ -0,0 +1,7 @@
+const openSidenav = () => {
+    document.querySelector("#side-nav").style.display = "flex"
+}
+
+const closeSidenav = () => {
+    document.querySelector("#side-nav").style.display = "none"
+}
